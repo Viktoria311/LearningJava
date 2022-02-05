6e5731ee17c54c4c412c26592e9dcfff7ba1997a
Victoria <kotova-viktorija@mail.ru>
2022-02-05 17:02:22 +0300

    beauty

--- src/main/java/lesson01/Lesson01.java
+++ src/main/java/lesson01/Lesson01.java
@@ -4,17 +4,16 @@ public class Lesson01 {
     public static void main(String[] args) {
 
         boolean yes = true;
-        char alpfa = 'V';
-        bite age = 27;
+        char alpha = 'V';
+        byte age = 27;
         short x = 0;
         int quantity = 18;
         const long MILLION = 1000000L;
         const float PI = 3.14f;
         double coins = 0.463;
-        String my_name = "Victoria";
+        String myName = "Victoria";
 
-        float func1(float a, float b, float c, float d)
-        {
+        float func1(float a, float b, float c, float d) {
             return  a * (b + (c / d));
         }
 
@@ -26,8 +25,7 @@ public class Lesson01 {
         System.out.println("Tre result of a function func1: ");
         System.out.println(func1(zx, nb, bg, jd));
 
-        boolean func2(int a, int b)
-        {
+        boolean func2(int a, int b) {
             if ( a+b >= 10 && a+b <= 20) return true;
             else return false;
         }
@@ -37,31 +35,24 @@ public class Lesson01 {
         System.out.println("The sum of a and b is between 10 and 20? ");
         System.out.println(func2(a, b));
 
-        void func3(int num)
-        {
-            if (n >= 0) System.out.println("This num is >= 0.");
-            else System.out.println("This num is less than 0.");
+        void func3(int number) {
+            if (number >= 0) System.out.println("This number is >= 0.");
+            else System.out.println("This number is less than 0.");
         }
 
-        boolean isLessThanZero(int num)
-        {
-            if (num<0) return true;
+        boolean isLessThanZero(int number) {
+            if (number<0) return true;
             else return false;
         }
-        void func4(String my_name)
-        {
+        void func4(String myName) {
             String message = "Привет, ";
-            message += my_name;
-            System.out.println(message);
+            System.out.println(message+myName);
         }
 
-        void func5(int year)
-        {
-            if ( year%4 == 0)
-            {
+        void func5(int year) {
+            if ( year%4 == 0) {
                 if (year % 100  != 0) System.out.println("високосный");
-                else
-                {
+                else {
                     if (year % 400 == 0) System.out.println("високосный");
                     else System.out.println("не високосный");
                 }
